02e1ea51e5ed5b657ba6b74735837275988e0d4f
mdnahidhossain0 <mdnahidooo730@gmail.com>
2026-07-29 23:34:06 +0600

    DT

---
+++ Decission_Trees/decission_trees.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = None
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+#class DecisionTree:
+ #   def __init__(self):
